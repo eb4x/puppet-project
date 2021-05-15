597a23add6ee838762d2580667e2bf61f8411a12
Erik Berg <github@slipsprogrammor.no>
2021-05-15 10:02:25 +0000

    get/install PDK

--- centos-7-setup.sh
+++ centos-7-setup.sh
@@ -6,3 +6,7 @@ sudo rpm -Uvh https://yum.puppet.com/puppet5-release-el-7.noarch.rpm
 sudo yum install -y puppet-agent
 
 sudo /opt/puppetlabs/puppet/bin/gem install r10k
+
+# Get PDK
+wget --content-disposition 'https://pm.puppet.com/cgi-bin/pdk_download.cgi?dist=el&rel=7&arch=x86_64&ver=latest'
+sudo yum install ./pdk-*.rpm
