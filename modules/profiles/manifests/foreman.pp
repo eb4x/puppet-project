dd284a456951e626b90d4a15f577e8edfc1895f7
Erik Berg <github@slipsprogrammor.no>
2021-10-25 20:44:16 +0000

    firewall rules

--- modules/profiles/manifests/foreman.pp
+++ modules/profiles/manifests/foreman.pp
@@ -9,4 +9,47 @@ class profiles::foreman {
   }
 
   include ::foreman
+
+  firewall { '190 foreman accept http/https':
+    dport => [80, 443],
+    proto => 'tcp',
+    #action => 'accept',
+  }
+
+  firewall { '191 foreman accept dns':
+    dport => 53,
+    proto => ['tcp', 'udp'],
+    #action => 'accept'
+  }
+
+  firewall { '192 foreman accept dhcp server':
+    dport => 67,
+    proto => 'udp',
+    #action => 'accept',
+  }
+
+  firewall { '193 foreman accept dhcp client':
+    chain => 'OUTPUT',
+    dport => 68,
+    proto => 'udp',
+    #action => 'accept',
+  }
+
+  firewall { '194 foreman accept tftp':
+    dport => 69,
+    proto => 'udp',
+    #action => 'accept',
+  }
+
+  firewall { '195 foreman accept puppet':
+    dport => 8140,
+    proto => 'tcp',
+    #action => 'accept',
+  }
+
+  firewall { '196 foreman accept https_proxy':
+    dport => 8443,
+    proto => 'tcp',
+    #action => 'accept',
+  }
 }
