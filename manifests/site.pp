08a5c1c529fdae793701b477d2c99bfcc0d0be2f
Erik Berg <github@slipsprogrammor.no>
2021-11-29 21:44:59 +0000

    include role on short_name

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,1 +1,16 @@
-lookup('classes', {merge => unique}).include
+$verified_certname = $::trusted['certname']
+$fqdn_array        = split($::verified_certname, '\.')
+$short_name        = $::fqdn_array[0]
+$short_name_array  = split($::short_name, '-')
+
+$location          = $::short_name_array[0]
+$role              = $::short_name_array[1]
+
+# dumb test
+if size($short_name_array) == 3 {
+  include role::$role
+} else if $short_name == "foreman" {
+  include role::admin
+}
+
+#lookup('classes', {merge => unique}).include
